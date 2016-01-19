3d855078c7e1f44bf6f165fb6d78ed2b0cf1a538
shavmark <mark@shavlik.us>
2016-01-19 10:42:46 -0600

    basic kinect helpers

---
+++ src/kinect.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "ofMain.h"
+#include "Kinect.Face.h"
+
+namespace From2552Software {
+
+	class Kinect {
+	public:
+		Kinect() {
+			pSensor = nullptr;
+		}
+		void open();
+		friend class KinectFaces;
+		friend class KinectFace;
+	private:
+		IKinectSensor* pSensor;
+		IColorFrameSource* pColorSource;
+		IBodyFrameSource* pBodySource;
+		IColorFrameReader* pColorReader;
+		IBodyFrameReader* pBodyReader;
+		IFrameDescription* pDescription;
+		ICoordinateMapper* pCoordinateMapper;
+		// Color Table
+		vector<ofColor> colors;
+
+		template<class Interface> static void SafeRelease(Interface *& pInterfaceToRelease)
+		{
+			if (pInterfaceToRelease != NULL) {
+				pInterfaceToRelease->Release();
+				pInterfaceToRelease = NULL;
+			}
+		}
+
+	};
+}
